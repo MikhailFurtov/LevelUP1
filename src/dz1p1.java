34761f02a38d015bcfeeb5253f4e9485d67c0108
Mikhalian <mail5690@mail.ru>
2021-11-26 15:45:19 +0300

    dz

---
+++ src/dz1p1.java
@@ -0,0 +1,11 @@
+public class dz1p1 {
+    public static void main(String [] arg) {
+        int a = 3765;
+        int b = 145;
+        System.out.println(a - b );
+        System.out.println(a + b );
+        System.out.println(a / b );
+        System.out.println(a % b );
+        System.out.println(a * b );
+    }
+}
---
+++ src/dz1p2.java
@@ -0,0 +1,8 @@
+public class dz1p2 {
+    public static void main(String [] arg) {
+        int a = 3;
+        int b = 9;
+        int x = (b/a);
+        System.out.println("3x+9=0 x="+ x);
+    }
+}
