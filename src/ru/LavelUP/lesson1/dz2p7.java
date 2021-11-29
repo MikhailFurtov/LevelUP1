88060cf5083a39aa8f6b1296f92d003aa68ddf21
Mihalian <mail5690@mail.ru>
2021-11-29 21:26:19 +0300

    all

---
+++ src/ru/LavelUP/lesson1/dz2p7.java
@@ -0,0 +1,12 @@
+package ru.LavelUP.lesson1;
+
+public class dz2p7 {
+    public static void main(String[] args) {
+        int[] mas = new int[55];
+        for (int i = 1, k= 1; k <=55; k++,k+=2) {
+            System.out.println(k);
+        }
+
+
+    }
+}
