f565bd42bc5e6eb2c9c45859ff64add6e05517f9
Mihalian <mail5690@mail.ru>
2021-11-30 00:36:26 +0300

    all

--- src/ru/LavelUP/lesson1/dz2p6.java
+++ src/ru/LavelUP/lesson1/dz2p6.java
@@ -3,7 +3,7 @@ package ru.LavelUP.lesson1;
 public class dz2p6 {
     public static void main(String[] args) {
         int k = 1000;
-        while (k<10000)
+        while (k<9997)
         System.out.println(k+=3);
     }
 }
--- src/ru/LavelUP/lesson1/dz2p8.java
+++ src/ru/LavelUP/lesson1/dz2p8.java
@@ -2,7 +2,6 @@ package ru.LavelUP.lesson1;
 
 public class dz2p8 {
     public static void main(String[] args) {
-        int[] mas = new int[90];
         for (int i=1,k=90;k>0;i++,k-=5){
             System.out.println(k);
         }
